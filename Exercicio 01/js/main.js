616705480f7b2f85390258396b7fb005d4e13c56
PeuHen <165822649+PeuHen@users.noreply.github.com>
2024-09-02 11:21:36 -0300

    feat: concluido

--- Exercicio 01/js/main.js
+++ Exercicio 01/js/main.js
@@ -3,54 +3,70 @@ var valor2 = document.getElementById("valor2");
 var resultado = document.getElementById("resultado");
 
 
+function converter() {
+  let n1 = Number(valor1.value);
+  let n2 = Number(valor2.value);
+  return { n1, n2 };
+}
+
+
+function calculo(operador) {
+  const { n1, n2 } = converter(); 
+
+  
+  if (!verificador(n1, n2)) {
+    return; 
+  }
+
+  let res;
+
+ 
+  switch (operador) {
+    case "+":
+      res = n1 + n2;
+      break;
+    case "-":
+      res = n1 - n2;
+      break;
+    case "*":
+      res = n1 * n2;
+      break;
+    case "/":
+      if ( n1 !== 0 && n2 !== 0) {
+        res = n1 / n2;
+      } else {
+        alert("Divisão por 0 não é possível.");
+        return; 
+      }
+      break;
+    default:
+      alert("Operador inválido.");
+      return;
+  }
+
+  resultado.innerText = res;
+}
+
+// Função para verificar se os campos estão preenchidos
+function verificador() {
+  if (valor1.value === '' || valor2.value === '') {
+    alert("Por favor, preencha os campos com números válidos.");
+    return false; 
+  }
+  return true; 
+}
+
+function bloquearLetras(tecla) {
+  if (  tecla.charCode >= 65 && tecla.charCode <=126)  { 
+    alert("Digite apenas números.");
+    return false;
+  }
+}
+
+
 function Limpar() {
   valor1.value = "";
   valor2.value = "";
   resultado.innerText = 0; 
   valor1.focus();
-}
-
-function calculo(operador) {
- 
-  let n1 = Number(valor1.value);
-  let n2 = Number(valor2.value);
-  let res = Number();
-    // verificador()
-  if (operador === "+") {
-    res = n1 + n2;
-  } else if (operador === "-") {
-    res = n1 - n2;
-  } else if (operador === "*") {
-    res = n1 * n2;
-  } else if (operador === "/") {
-    if (n2 !== 0) {
-      res = n1 / n2;
-    } else {
-      alert("Divisão por 0 não é possível");
-      return; 
-    }
-  } else {
-    alert("Operador inválido");
-    return;
-  }
-
-  
-  resultado.innerText = res;
-
-}
-
-function bloquearLetras(tecla){
-  console.log(tecla.charCode);
-  if ( tecla.charCode >= 65 && tecla.charCode <=112){
-    alert("Digite so numeros")
-    return false;
-  }
-}
-
-function verificador(){
-  if (n1 || n2 == ''){
-    alert("preenha os campos")
-  } else{
-    alert("td crto")
-  }
 }
